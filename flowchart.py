99f7f3b8aa1bae08b2ca21144d5d117110f028bb
Michael Scott <thebluenowhere@protonmail.com>
2022-05-31 15:33:22 +0100

    initial push

---
+++ flowchart.py
@@ -0,0 +1,5 @@
+is_raining = True
+have_umbrella = False
+
+while is_raining and not have_umbrella: 
+    print("Wait a while")
---
+++ password.py
@@ -0,0 +1,10 @@
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Michael':
+        continue
+    print(f'Hello, {name}. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access granted.')
